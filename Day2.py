f4d4ee3c5ffbbb3e66a794ed95f0e97fa31d992c
Filip Kozik <kozikfilip@gmail.com>
2017-12-02 17:43:56 +0100

    so that's how it's supposed to work

--- Day2.py
+++ Day2.py
@@ -4,24 +4,12 @@ the checksum is the sum of all of these differences.
 """
 
 
-def rowDiff(row):
-    minimum = 10
-    maximum = 0
-    for num in row:
-        if int(num) < minimum: minimum = int(num)
-        if int(num) > maximum: maximum = int(num)
-    return maximum - minimum
-
-
-def allRows(fileobject):
-    data = fileobject.read()
-    rowList = data.split()
-    print(rowList)
-    rowDiffs = []
-    for num in rowList:
-        rowDiffs.append(rowDiff(num))
-    return sum(rowDiffs)
-
+def checksum(fileobject):
+    sum = 0;
+    for line in fileobject.readlines():
+        lineNum = list(map(int, line.split()))
+        sum += max(lineNum) - min(lineNum)
+    return sum
 
 file = open("inputDay2.txt")
-print(allRows(file))
+print(checksum(file))
