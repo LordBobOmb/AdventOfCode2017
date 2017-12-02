af6faf07f448e058f535a3abcd40818fa0715eaa
Filip Kozik <kozikfilip@gmail.com>
2017-12-02 17:55:00 +0100

    second part

--- Day2.py
+++ Day2.py
@@ -4,12 +4,23 @@ the checksum is the sum of all of these differences.
 """
 
 
-def checksum(fileobject):
+def checksum(lines):
     sum = 0;
-    for line in fileobject.readlines():
+    for line in lines:
         lineNum = list(map(int, line.split()))
         sum += max(lineNum) - min(lineNum)
     return sum
 
+def evenlyDivisibleSum(line):
+    sum = 0;
+    for line in lines:
+        lineNum = list(map(int, line.split()))
+        for num1 in lineNum:
+            for num2 in lineNum:
+                if (num1 > num2 and num1 % num2 == 0): sum += int(num1 / num2)
+    return sum
+
 file = open("inputDay2.txt")
-print(checksum(file))
+lines = file.readlines()
+print(checksum(lines))
+print(evenlyDivisibleSum(lines))
